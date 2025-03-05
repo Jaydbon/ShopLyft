d29bfdb8389a452c4e8777c4c76af0b375caf916
Jayden <jaydenbarber05@gmail.com>
2025-03-04 22:54:19 -0500

    Added delete button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from backEndShopLyft import ClothingCatalogue # changed from "from ClothingItem import ClothingCatalogue" -Jordyn
 
 app = Flask(__name__)
@@ -19,11 +19,18 @@ def admin():
     return render_template('admin.html', cards=info)
 
 
-# @app.route('/delete/<item_id>', methods=['POST'])
-# def deleteItem(item_id):
-#     if item_id in info:
-#         del info[item_id]
-#     return render_template('admin.html', cards=info)
+@app.route('/delete/<item_id>', methods=['POST'])
+def deleteItem(item_id):
+    global info
+    item_to_delete = None
+    for item in catalogue.items:
+        if item.name == item_id:
+            catalogue.remove_item(item)
+
+    # Remove the item from the info list
+    info = [item for item in info if item['name'] != item_id]
+    print(item_id)
+    return redirect(url_for('admin'))
 
 if __name__ == '__main__':
     app.run(debug=True)
