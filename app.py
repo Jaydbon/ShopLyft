f1058b5487c9a3101b552887acbc6033bf3cf97f
Wingfung Hu <wingfung.hu@gmail.com>
2025-03-05 16:00:49 -0500

    add.html works with backend properly

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from backEndShopLyft import ClothingCatalogue # changed from "from ClothingItem import ClothingCatalogue" -Jordyn
+from backEndShopLyft import ClothingItem, ClothingCatalogue # changed from "from ClothingItem import ClothingCatalogue" -Jordyn
 
 app = Flask(__name__)
 
@@ -18,6 +18,10 @@ def staff():
 def admin():
     return render_template('admin.html', cards=info)
 
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
 
 @app.route('/delete/<item_id>', methods=['POST'])
 def deleteItem(item_id):
@@ -30,6 +34,26 @@ def deleteItem(item_id):
     info = [item for item in info if item['name'] != item_id]
     print(item_id)
     return redirect(url_for('admin'))
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    global info
+    # Create a dictionary to store the form data
+    name = request.form.get('name')
+    size = request.form.get('size')
+    colour = request.form.get('colour')
+    gender = request.form.get('gender')
+    price = float(request.form.get('price'))
+    quantity = int(request.form.get('stock'))  # 'stock' in the form corresponds to 'quantity' in the backend
+    brand = request.form.get('brand')
+    image = request.form.get('image', "")
+
+    # Create a ClothingItem object
+    new_item = ClothingItem(name, size, colour, gender, price, quantity, brand, image)
+
+    result = catalogue.add_item(new_item)
+
+    return redirect(url_for('add'))
 
 if __name__ == '__main__':
     app.run(debug=True)
